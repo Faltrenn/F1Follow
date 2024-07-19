47b26af85d948807bfae29c932e0ad523639ef8f
Emanuel Alves <manelzaum@icloud.com>
2024-07-19 20:31:23 -0300

    Initial Commit

---
+++ F1Follow/F1FollowApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1FollowApp.swift
+//  F1Follow
+//
+//  Created by Emanuel on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct F1FollowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
