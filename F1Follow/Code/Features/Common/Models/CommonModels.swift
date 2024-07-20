c10c48888a0665ee7d44311dd953a0628e96f01f
Emanuel Alves <manelzaum@icloud.com>
2024-07-19 22:37:04 -0300

    remove some driver infos.

--- F1Follow/Code/Features/Common/Models/CommonModels.swift
+++ F1Follow/Code/Features/Common/Models/CommonModels.swift
@@ -12,32 +12,28 @@ struct Driver: Codable {
     let broadcastName: String
     let countryCode: String
     let driverNumber: Int
-    let firstName: String
     let fullName: String
-    let headshotUrl: String
-    let lastName: String
+    let headshotURL: String
     let nameAcronym: String
     let teamColour: String
     let teamName: String
-    
+
     enum CodingKeys: String, CodingKey {
         case broadcastName  = "broadcast_name"
-        case countryCode   = "country_code"
-        case driverNumber  = "driver_number"
-        case firstName     = "first_name"
-        case fullName      = "full_name"
-        case headshotUrl   = "headshot_url"
-        case lastName      = "last_name"
-        case nameAcronym   = "name_acronym"
-        case teamColour    = "team_colour"
-        case teamName      = "team_name"
+        case countryCode    = "country_code"
+        case driverNumber   = "driver_number"
+        case fullName       = "full_name"
+        case headshotURL    = "headshot_url"
+        case nameAcronym    = "name_acronym"
+        case teamColour     = "team_colour"
+        case teamName       = "team_name"
     }
 }
 
 
 extension Color {
     init(hex: String) {
-        let rgb: UInt64 = UInt64(hex, radix: 16)!
+        let rgb: UInt64 = UInt64(hex, radix: 16) ?? 0
         
         let redValue = Double((rgb >> 16) & 0xFF) / 255.0
         let greenValue = Double((rgb >> 8) & 0xFF) / 255.0
