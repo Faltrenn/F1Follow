da7820c3a5add7b24f837def5dfbd8fad5131706
Emanuel Alves <manelzaum@icloud.com>
2024-07-28 13:23:36 -0300

    add: session

--- F1Follow/ContentView.swift
+++ F1Follow/ContentView.swift
@@ -63,9 +63,29 @@ struct GPCard: View {
     }
 }
 
+struct Session: Codable {
+    let dateStart: Date
+    let sessionKey: Int
+    let sessionName, sessionType: String
+
+    enum CodingKeys: String, CodingKey {
+        case dateStart = "date_start"
+        case sessionKey = "session_key"
+        case sessionName = "session_name"
+        case sessionType = "session_type"
+    }
+}
+
 struct ContentView: View {
     @State var year: Int = 2024
-    @ObservedObject var cVM = ContentViewModel()
+//    @ObservedObject var cVM = ContentViewModel()
+    @State var gps: [GP] = [
+            GP(circuitKey: 1, circuitShortName: "Monza", countryCode: "IT", countryKey: 39, location: "Monza, Italy", meetingKey: 101, meetingOfficialName: "Italian Grand Prix", year: 2024),
+            GP(circuitKey: 2, circuitShortName: "Silverstone", countryCode: "GB", countryKey: 44, location: "Silverstone, UK", meetingKey: 102, meetingOfficialName: "British Grand Prix", year: 2024),
+            GP(circuitKey: 3, circuitShortName: "Spa", countryCode: "BE", countryKey: 32, location: "Spa, Belgium", meetingKey: 103, meetingOfficialName: "Belgian Grand Prix", year: 2024),
+            GP(circuitKey: 4, circuitShortName: "Suzuka", countryCode: "JP", countryKey: 81, location: "Suzuka, Japan", meetingKey: 104, meetingOfficialName: "Japanese Grand Prix", year: 2024),
+            GP(circuitKey: 5, circuitShortName: "Interlagos", countryCode: "BR", countryKey: 55, location: "São Paulo, Brazil", meetingKey: 105, meetingOfficialName: "Brazilian Grand Prix", year: 2024)
+        ]
     
     var body: some View {
         NavigationStack {
@@ -81,8 +101,42 @@ struct ContentView: View {
                 }
                 ScrollView {
                     VStack(alignment: .leading) {
-                        ForEach(Array(zip(cVM.gps.indices, cVM.gps)), id: \.1.circuitKey) { c, gp in
+                        ForEach(Array(zip(gps.indices, gps)), id: \.1.circuitKey) { c, gp in
                             NavigationLink {
+                                @State var sessions: [Session] = [
+                                        Session(dateStart: Date(), sessionKey: 201, sessionName: "Practice 1", sessionType: "Practice"),
+                                        Session(dateStart: Date(), sessionKey: 202, sessionName: "Practice 2", sessionType: "Practice"),
+                                        Session(dateStart: Date(), sessionKey: 203, sessionName: "Practice 3", sessionType: "Practice"),
+                                        Session(dateStart: Date(), sessionKey: 204, sessionName: "Qualifying", sessionType: "Qualifying"),
+                                        Session(dateStart: Date(), sessionKey: 205, sessionName: "Race", sessionType: "Race")
+                                    ]
+                                VStack(alignment: .leading) {
+                                    ForEach(sessions, id: \.sessionKey) { session in
+                                        NavigationLink {
+                                            Text("boa")
+                                        } label: {
+                                            HStack {
+                                                VStack {
+                                                    Text("Dia")
+                                                    Text("Mes")
+                                                        .padding(.horizontal)
+                                                        .background(.gray)
+                                                }
+                                                VStack(alignment: .leading) {
+                                                    Text(session.sessionName)
+                                                        .font(.title2)
+                                                        .bold()
+                                                    Text("Veja os detalhes")
+                                                        .font(.caption2)
+                                                }
+                                            }
+                                            .tint(.primary)
+                                        }
+                                    }
+                                }
+                                .onAppear {
+                                    
+                                }
                             } label: {
                                 GPCard(gp: gp, round: c+1)
                             }
